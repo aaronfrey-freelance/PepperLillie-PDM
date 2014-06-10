baa670d5fd475dba2a092c20d9e23ca0b503f3e9
Aaron <aaron@facultycreative.com>
2014-06-10 19:00:41 -0400

    Add search bar to the top nav bar

--- wp-content/themes/pdm/templates/header-top-navbar.php
+++ wp-content/themes/pdm/templates/header-top-navbar.php
@@ -16,6 +16,13 @@
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
+      <form role="search" method="get" class="navbar-form navbar-right" action="<?php echo home_url('/'); ?>">
+        <div class="form-group">
+          <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
+          <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="form-control" placeholder="Search...">
+        </div>
+        <button type="submit" class="hide btn btn-default"><?php _e('Search', 'roots'); ?></button>
+      </form>
     </nav>
   </div>
 </header>
