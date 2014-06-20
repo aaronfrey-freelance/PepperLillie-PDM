a283ce58195ab6b9478e9b9c9885602e6f8301a2
Aaron <aaron@facultycreative.com>
2014-06-20 16:30:18 -0400

    Installed fonts

--- wp-content/themes/pdm/index.php
+++ wp-content/themes/pdm/index.php
@@ -6,7 +6,7 @@
   </div>
 <?php endif; ?>
 
-<?php query_posts($query_string."&orderby=date&order=ASC"); ?>
+<?php if (is_category('about') || is_category('projects')) query_posts($query_string."&orderby=date&order=ASC"); ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'category'); ?>
 <?php endwhile; ?>
--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '273375bf3410b753b51112a100072a01');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '642d882a766b2013c3e88ce4a4a0c4c1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
