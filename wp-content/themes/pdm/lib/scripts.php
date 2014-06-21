c6922b16813ff94d7e2a3aff59177017fb62ee53
Aaron <aaron@facultycreative.com>
2014-06-21 11:03:14 -0400

    Styled footer

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '642d882a766b2013c3e88ce4a4a0c4c1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c380c30f8b75d2f72fbc1df7cdfa821');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/pdm/templates/footer.php
+++ wp-content/themes/pdm/templates/footer.php
@@ -1,20 +1,35 @@
 <footer class="content-info" role="contentinfo">
+
   <div class="container">
 
-		<ul class="pull-left">
-			<li><?php echo get_option('address1'); ?></li>
-			<li><?php echo get_option('address2'); ?></li>
-			<li><?php echo get_option('address3'); ?></li>
-			<li><?php echo get_option('phonenumber'); ?></li>
-		</ul>
+		<div class="row">
 
-		<div class="social-footer pull-right">
-			<a href="<?php echo get_option('linkedinurl'); ?>" target="_blank" title="Go to LinkedIn" class="linkedin">LinkedIn</a>
-			<a href="<?php echo get_option('facebookurl'); ?>" target="_blank" title="Go to Facebook" class="facebook">Facebook</a>
-			<a href="<?php echo get_option('youtubeurl'); ?>" target="_blank" title="Go to Youtube" class="youtube">Youtube</a>
+			<div class="col-md-9 col-sm-10 col-xs-12">
+				<div class="address-footer">
+					<span class="first"><?php echo get_option('address1'); ?></span>
+					<span class="border"></span>
+					<span><?php echo get_option('address2'); ?></span>
+					<span class="border"></span>
+					<span><?php echo get_option('address3'); ?></span>
+					<span class="border"></span>
+					<span><?php echo get_option('phonenumber'); ?></span>
+					<a href="#" class="ftp hidden-sm hidden-xs">FTP</a>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-2 col-xs-12">
+				<div class="social-footer">
+					<span class="hidden-sm">Connect</span>
+					<a href="<?php echo get_option('linkedinurl'); ?>" target="_blank" title="Go to LinkedIn" class="linkedin">LinkedIn</a>
+					<a href="<?php echo get_option('facebookurl'); ?>" target="_blank" title="Go to Facebook" class="facebook">Facebook</a>
+					<a href="<?php echo get_option('youtubeurl'); ?>" target="_blank" title="Go to Youtube" class="youtube">Youtube</a>
+				</div>
+			</div>
+
 		</div>
 
   </div>
+
 </footer>
 
 <?php wp_footer(); ?>
