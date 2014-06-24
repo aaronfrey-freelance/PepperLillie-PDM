a590fd6f95eb62a226c293b5e8c53a8f87ce102c
Aaron <aaron@facultycreative.com>
2014-06-23 20:49:12 -0400

    Finished styling header

--- wp-content/themes/pdm/templates/header-top-navbar.php
+++ wp-content/themes/pdm/templates/header-top-navbar.php
@@ -1,12 +1,18 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
+
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+
+      <a href="#" class="pull-right hidden-lg hidden-md">
+        <span class="glyphicon glyphicon-search"></span>
+      </a>
+
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"></a>
     </div>
 
@@ -14,6 +20,7 @@
 
       <form role="search" method="get" class="navbar-form navbar-right hidden-sm hidden-xs" action="<?php echo home_url('/'); ?>">
         <div class="form-group">
+          <span class="glyphicon glyphicon-search"></span>
           <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
           <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="form-control" placeholder="Search...">
         </div>
