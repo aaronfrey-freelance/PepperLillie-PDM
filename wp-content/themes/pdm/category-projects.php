01661d8de968ff8c3aec6e49d6a45a71f08979c1
Aaron <aaron@facultycreative.com>
2014-07-06 13:55:19 -0400

    Style contact page

---
+++ wp-content/themes/pdm/category-projects.php
@@ -0,0 +1,25 @@
+<div class="projects-index">
+
+<?php query_posts($query_string."&orderby=date&order=ASC"); ?>
+<?php while (have_posts()) : the_post(); ?>
+
+	<div class="col-sm-4">
+
+		<a href="<?php the_permalink(); ?>">
+		  <article <?php post_class(); ?>>
+			  <div class="entry-summary">
+			    <div class="project-image">
+			    	 <?php the_post_thumbnail(); ?>
+			    </div>
+			    <div class="entry-title">
+			    	<?php the_title(); ?>
+			    </div>
+			  </div>
+			</article>
+		</a>
+
+	</div>
+
+<?php endwhile; ?>
+
+</div>
--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e4d12fc2cf22ed470381a0e9adcaa45b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3a7e81de0a4cef23f9721c346ee8fce6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
