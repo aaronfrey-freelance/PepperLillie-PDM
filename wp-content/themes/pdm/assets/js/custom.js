293df2ccd37d118e41e9c4fc3bb7faaa706b3ad2
Aaron <aaron@facultycreative.com>
2014-07-13 14:13:55 -0400

    Fix issue with sidebar not resizing properly

--- wp-content/themes/pdm/assets/js/custom.js
+++ wp-content/themes/pdm/assets/js/custom.js
@@ -1,10 +1,22 @@
 $(function() {
 
-  var center_content_height = $('.center-content').outerHeight();
-  var padding_bottom = $('.center-content').css('padding-bottom');
+  function resizeSidebar() {
+    var center_content_height = $('.center-content').outerHeight();
+    var padding_bottom = $('.center-content').css('padding-bottom');
 
-  $('aside.sidebar').height(center_content_height);
-  $('aside.sidebar').css('margin-bottom', -parseInt(padding_bottom, 10));
+    if(center_content_height > $('aside.sidebar').outerHeight()) {
+      $('aside.sidebar').height(center_content_height);
+      $('aside.sidebar').css('margin-bottom', -parseInt(padding_bottom, 10));
+    } else {
+      $('aside.sidebar').height('auto');
+    }
+  }
+
+  resizeSidebar();
+
+  $(window).resize(function() {
+    resizeSidebar();
+  });
 
   /* Teaser Links */
   $('#teaser-links a').on('click', function(e) {
--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c4a088e983fe9a1f8ac4c62107fc1aae');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a857191fb8e8d081ba71a21a0c0328f4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
