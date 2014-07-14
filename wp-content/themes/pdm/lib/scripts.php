da1b96dbb4f34baafbbad3d23b54d4399653b2fc
Aaron <aaron@facultycreative.com>
2014-07-14 06:41:56 -0400

    Add full screen background to the projects landing page

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '98fcc5f7061ebda1622da4f4379837cb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8310548b18f425e124a62ffa8d1572e6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
