7cb408fd5a9498469325db50e89146b8509ee812
Aaron <aaron@facultycreative.com>
2014-07-17 15:53:14 -0400

    Fix Careers

---
+++ wp-content/themes/pdm/single-job_listing.php
@@ -0,0 +1,9 @@
+<?php get_template_part('templates/content', 'page'); ?>
+
+<br><br>
+
+<p>
+	<em>Philadelphia D&M, Inc. is an equal opportunity/affirmative action employer. Individuals who require reasonable accomodations under the Americans with Disbilities Act in order to participate in the search process should notify our office directly at <?php echo get_option('phonenumber'); ?>.</em>
+</p>
+
+<br><br>
