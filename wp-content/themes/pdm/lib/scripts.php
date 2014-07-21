7771a683a9750a9c58c3f0d9d7deb2f987ff6436
Aaron <aaron@facultycreative.com>
2014-07-21 01:35:23 -0400

    Fix drop down menu

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5b5ad507c1f0fb588ed3537b1c1b76f4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '85790b1b4593ee665516a02e2232e572');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
