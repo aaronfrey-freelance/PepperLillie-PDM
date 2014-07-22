cd783d5b7e9029065561b40c3667eede4fde04e3
Aaron <aaron@facultycreative.com>
2014-07-22 19:01:53 -0400

    Remove default styling of telephone numbers on mobile safari

--- wp-content/themes/pdm/templates/head.php
+++ wp-content/themes/pdm/templates/head.php
@@ -6,6 +6,9 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- Remove default styling of phone numbers on mobile -->
+  <meta name="format-detection" content="telephone=no">
+
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
