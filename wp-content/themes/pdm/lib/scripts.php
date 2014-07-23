fdb73d794ad2cc4b4a53ce3075ce3bf7ef740005
Aaron <aaron@facultycreative.com>
2014-07-23 02:33:09 -0400

    Padding

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '30b35a5bf3f812fd8a0674b683411748');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '200038f1fe3cd20bf4d90227a6147296');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
