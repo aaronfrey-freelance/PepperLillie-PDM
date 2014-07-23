355fdbc92e4afc1d4689535d3ac742b151a9c2b5
Aaron <aaron@facultycreative.com>
2014-07-23 13:08:52 -0400

    Add favicon

--- wp-content/themes/pdm/templates/head.php
+++ wp-content/themes/pdm/templates/head.php
@@ -11,5 +11,8 @@
 
   <?php wp_head(); ?>
 
+	<link rel="icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
