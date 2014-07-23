344e8f2989bf5a5ecb9390c37c2c9bde254e6d56
Aaron <aaron@facultycreative.com>
2014-07-22 20:55:25 -0400

    Make job border darker

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e0b3b6c56539444b59a3738b00120af1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd06a14f8eb4564a279bac249e1d6eba6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
