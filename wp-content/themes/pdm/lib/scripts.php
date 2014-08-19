56d8e7d759f9f387c6409d32c4d2afafcecf2e37
Aaron <aaron@facultycreative.com>
2014-08-19 10:59:11 -0400

    Sync up styles

--- wp-content/themes/pdm/lib/scripts.php
+++ wp-content/themes/pdm/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'aabe1e252ee14ca9747edaf4906a4a4d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'af6a8d48d04f950801ffb287de230358');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
